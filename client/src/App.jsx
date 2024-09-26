5f7910f2ca1a909fa521c018eb95b49ca309cab3
Guille <162798757+Gbonn7@users.noreply.github.com>
2024-09-26 13:41:58 +0200

    feature(BB2.2) Add frontend for the login screen that redirects the user to the home page. (#5)
    * feature(BB2.2) Add frontend for the login screen that redirects the user to the home landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './login.jsx';
+import Home from './home.jsx';
 
 const App = () => {
   return (
-    <div className='bg-black text-white'>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App
---
+++ client/src/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-2xl font-bold">Esto es la página de inicio.</h1>
+        </div>
+    );
+};
+
+export default Home;
