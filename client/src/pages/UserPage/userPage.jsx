fe291e6fc9847d312e59c55ab01a335896d7f861
Gbonn7 <gbonninlettering@gmail.com>
2024-10-01 16:53:22 +0200

    Add simple CSS to user information page

--- client/src/pages/UserPage/userPage.jsx
+++ client/src/pages/UserPage/userPage.jsx
@@ -1,41 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import UserService from '@services/user/user.service';
 
-const UserPage = ()=> {
-    const [userData, setUserData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+const UserPage = () => {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const getUserData = async () => {
-            try {
-                const response = await UserService.fetchUserData();
-                setUserData(response);
-            } catch (err) {
-                setError('Error al obtener los datos del usuario.');
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await UserService.fetchUserData();
+        setUserData(response);
+      } catch (err) {
+        setError('Error al obtener los datos del usuario.');
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        getUserData();
-    },[]);
+    getUserData();
+  }, []);
 
-    if (loading) {
-        return <div>Cargando...</div>;
-    }
-    if (error) {
-        return <div>{error}</div>;
-    }
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
 
-    return(
-        <div>
-            <h1>Información del usuario</h1>
-            <p>Email: {userData.email}</p>
-            <p>Username: {userData.username}</p>
-            <img src={`../assets/${userData.profilePicture}`} alt="Profile picture" />
-        </div>
-    );
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="bg-white p-6 rounded shadow-md w-full max-w-sm">
+      <h1 className="text-2xl font-bold mb-6 text-center">Información del usuario</h1>
+      <p className="mb-4 text-gray-700">Email: {userData.email}</p>
+      <p className="mb-4 text-gray-700">Username: {userData.username}</p>
+      <img
+        src={`../assets/${userData.profilePicture}`}
+        alt="Profile picture"
+        className="block mx-auto rounded"
+      />
+    </div>
+  </div>
+  );
 };
 
 export default UserPage;
