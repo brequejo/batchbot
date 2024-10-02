950c3c0158257c1f22fca51b01ae4150ecfe5485
Bea Requejo <beatriz.requejo@bq.com>
2024-10-02 23:54:46 +0200

    Edit recipe url on the client to match the new structure in the server

--- client/src/services/recipes/recipes.service.js
+++ client/src/services/recipes/recipes.service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5555';
 
 const getUserRecipes = (userId) => {
-  const url = `${API_URL}/recipes/${userId}`;
+  const url = `${API_URL}/recipes?userId=${userId}`;
   return axios
     .get(url, {
       withCredentials: true,
